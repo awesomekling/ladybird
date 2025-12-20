awesomekling/ladybird

commit aec20e032b50892c27729b5c5899b9b4cf0967bb

LibJS: Add `FunctionObject::fast_is<BoundFunction>()`

Libraries/LibJS/Runtime/BoundFunction.h
@@ -36,9 +36,14 @@ private:
     ThrowCompletionOr<void> get_stack_frame_size(size_t& registers_and_constants_and_locals_count, size_t& argument_count) override;
     virtual void visit_edges(Visitor&) override;
 
+    virtual bool is_bound_function() const final { return true; }
+
     GC::Ptr<FunctionObject> m_bound_target_function; // [[BoundTargetFunction]]
     Value m_bound_this;                              // [[BoundThis]]
     Vector<Value> m_bound_arguments;                 // [[BoundArguments]]
 };
+
+template<>
+inline bool FunctionObject::fast_is<BoundFunction>() const { return is_bound_function(); }
 
 }

Libraries/LibJS/Runtime/FunctionObject.h
@@ -42,6 +42,9 @@ public:
 
     virtual FunctionParameters const& formal_parameters() const { VERIFY_NOT_REACHED(); }
 
+    template<typename T>
+    bool fast_is() const = delete;
+
 protected:
     explicit FunctionObject(Realm&, Object* prototype, MayInterfereWithIndexedPropertyAccess = MayInterfereWithIndexedPropertyAccess::No);
     explicit FunctionObject(Object& prototype, MayInterfereWithIndexedPropertyAccess = MayInterfereWithIndexedPropertyAccess::No);
@@ -50,6 +53,7 @@ protected:
 
 private:
     virtual bool is_function() const override { return true; }
+    virtual bool is_bound_function() const { return false; }
 };
 
 template<>

commit 1001020656a9f061a6f20f02d170bb06f5918354

LibJS: Remove FunctionObject::fast_is<T>() deletion
This broke fast is<ECMAScriptFunctionObject> and is<NativeFunction>.
By moving them up to Object, we get fast checks for these types again.

Regressed in aec20e032b50892c27729b5c5899b9b4cf0967bb.

Libraries/LibJS/Runtime/BoundFunction.h
@@ -46,6 +46,6 @@ private:
 };
 
 template<>
-inline bool FunctionObject::fast_is<BoundFunction>() const { return is_bound_function(); }
+inline bool Object::fast_is<BoundFunction>() const { return is_bound_function(); }
 
 }

Libraries/LibJS/Runtime/FunctionObject.h
@@ -44,9 +44,6 @@ public:
 
     virtual Utf16String name_for_call_stack() const = 0;
 
-    template<typename T>
-    bool fast_is() const = delete;
-
 protected:
     explicit FunctionObject(Realm&, Object* prototype, MayInterfereWithIndexedPropertyAccess = MayInterfereWithIndexedPropertyAccess::No);
     explicit FunctionObject(Object& prototype, MayInterfereWithIndexedPropertyAccess = MayInterfereWithIndexedPropertyAccess::No);
@@ -55,7 +52,6 @@ protected:
 
 private:
     virtual bool is_function() const override { return true; }
-    virtual bool is_bound_function() const { return false; }
 };
 
 template<>

Libraries/LibJS/Runtime/NativeFunction.h
@@ -76,7 +76,4 @@ private:
 template<>
 inline bool Object::fast_is<NativeFunction>() const { return is_native_function(); }
 
-template<>
-inline bool FunctionObject::fast_is<NativeFunction>() const { return is_native_function(); }
-
 }

Libraries/LibJS/Runtime/Object.h
@@ -215,6 +215,7 @@ public:
     virtual bool is_html_location() const { return false; }
 
     virtual bool is_function() const { return false; }
+    virtual bool is_bound_function() const { return false; }
     virtual bool is_promise() const { return false; }
     virtual bool is_error_object() const { return false; }
     virtual bool is_date() const { return false; }