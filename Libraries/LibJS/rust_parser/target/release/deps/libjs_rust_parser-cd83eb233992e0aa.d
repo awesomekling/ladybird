c7c659b526a0d78337b2a6adabc8b0ef77ffd10c
Andreas Kling <andreas@ladybird.org>
2026-02-10 20:19:50 +0100

    LibJS: Remove dead scope_analysis.rs stub from Rust parser
    The real scope analysis is implemented in scope_collector.rs.
    This file was never imported and contained only a TODO stub.

--- Libraries/LibJS/rust_parser/target/release/deps/libjs_rust_parser-cd83eb233992e0aa.d
+++ Libraries/LibJS/rust_parser/target/release/deps/libjs_rust_parser-cd83eb233992e0aa.d
@@ -1,6 +1,6 @@
-/home/kling/src/ladybird/Libraries/LibJS/rust_parser/target/release/deps/libjs_rust_parser-cd83eb233992e0aa.d: src/lib.rs src/ast_bridge.rs src/lexer.rs src/parser.rs src/parser/declarations.rs src/parser/expressions.rs src/parser/statements.rs src/token.rs
+/home/kling/src/ladybird/Libraries/LibJS/rust_parser/target/release/deps/libjs_rust_parser-cd83eb233992e0aa.d: src/lib.rs src/ast_bridge.rs src/lexer.rs src/parser.rs src/parser/declarations.rs src/parser/expressions.rs src/parser/statements.rs src/scope_collector.rs src/token.rs
 
-/home/kling/src/ladybird/Libraries/LibJS/rust_parser/target/release/deps/liblibjs_rust_parser-cd83eb233992e0aa.a: src/lib.rs src/ast_bridge.rs src/lexer.rs src/parser.rs src/parser/declarations.rs src/parser/expressions.rs src/parser/statements.rs src/token.rs
+/home/kling/src/ladybird/Libraries/LibJS/rust_parser/target/release/deps/liblibjs_rust_parser-cd83eb233992e0aa.a: src/lib.rs src/ast_bridge.rs src/lexer.rs src/parser.rs src/parser/declarations.rs src/parser/expressions.rs src/parser/statements.rs src/scope_collector.rs src/token.rs
 
 src/lib.rs:
 src/ast_bridge.rs:
@@ -9,4 +9,5 @@ src/parser.rs:
 src/parser/declarations.rs:
 src/parser/expressions.rs:
 src/parser/statements.rs:
+src/scope_collector.rs:
 src/token.rs:
