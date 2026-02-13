c89909588bdb45e40230f0850499d134530c5e46
Andreas Kling <andreas@ladybird.org>
2026-02-13 10:03:15 +0100

    Cleanup

--- Libraries/LibJS/rust_parser/target/release/deps/unicode_ident-f43ae609a1787e8e.d
+++
@@ -1,8 +0,0 @@
-/home/kling/src/ladybird/Libraries/LibJS/rust_parser/target/release/deps/unicode_ident-f43ae609a1787e8e.d: /home/kling/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/lib.rs /home/kling/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/tables.rs
-
-/home/kling/src/ladybird/Libraries/LibJS/rust_parser/target/release/deps/libunicode_ident-f43ae609a1787e8e.rlib: /home/kling/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/lib.rs /home/kling/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/tables.rs
-
-/home/kling/src/ladybird/Libraries/LibJS/rust_parser/target/release/deps/libunicode_ident-f43ae609a1787e8e.rmeta: /home/kling/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/lib.rs /home/kling/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/tables.rs
-
-/home/kling/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/lib.rs:
-/home/kling/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/tables.rs:
