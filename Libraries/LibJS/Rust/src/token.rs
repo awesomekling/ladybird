39f402e8b797be3d54809da82410a1bbbb961827
Andreas Kling <andreas@ladybird.org>
2026-02-16 17:53:03 +0100

    LibJS: Replace token match arms with const lookup tables in Rust
    Replace the 130-line category() match and 122-line name() match
    with const arrays indexed by the token type's u8 discriminant.

--- Libraries/LibJS/Rust/src/token.rs
+++ Libraries/LibJS/Rust/src/token.rs
@@ -151,265 +151,269 @@ pub enum TokenCategory {
     Identifier,
 }
 
+const TOKEN_COUNT: usize = 122;
+
+use TokenCategory::*;
+
+// Indexed by TokenType as u8. Order must match the TokenType enum.
+const TOKEN_CATEGORIES: [TokenCategory; TOKEN_COUNT] = [
+    Operator,        // Ampersand
+    Operator,        // AmpersandEquals
+    Operator,        // Arrow
+    Operator,        // Asterisk
+    Operator,        // AsteriskEquals
+    Keyword,         // Async
+    Keyword,         // Await
+    Number,          // BigIntLiteral
+    Keyword,         // BoolLiteral
+    Punctuation,     // BracketClose
+    Punctuation,     // BracketOpen
+    ControlKeyword,  // Break
+    Operator,        // Caret
+    Operator,        // CaretEquals
+    ControlKeyword,  // Case
+    ControlKeyword,  // Catch
+    Keyword,         // Class
+    Punctuation,     // Colon
+    Punctuation,     // Comma
+    Keyword,         // Const
+    ControlKeyword,  // Continue
+    Punctuation,     // CurlyClose
+    Punctuation,     // CurlyOpen
+    Keyword,         // Debugger
+    ControlKeyword,  // Default
+    Keyword,         // Delete
+    ControlKeyword,  // Do
+    Operator,        // DoubleAmpersand
+    Operator,        // DoubleAmpersandEquals
+    Operator,        // DoubleAsterisk
+    Operator,        // DoubleAsteriskEquals
+    Operator,        // DoublePipe
+    Operator,        // DoublePipeEquals
+    Operator,        // DoubleQuestionMark
+    Operator,        // DoubleQuestionMarkEquals
+    ControlKeyword,  // Else
+    Keyword,         // Enum
+    Invalid,         // Eof
+    Operator,        // Equals
+    Operator,        // EqualsEquals
+    Operator,        // EqualsEqualsEquals
+    Identifier,      // EscapedKeyword
+    Operator,        // ExclamationMark
+    Operator,        // ExclamationMarkEquals
+    Operator,        // ExclamationMarkEqualsEquals
+    Keyword,         // Export
+    Keyword,         // Extends
+    ControlKeyword,  // Finally
+    ControlKeyword,  // For
+    Keyword,         // Function
+    Operator,        // GreaterThan
+    Operator,        // GreaterThanEquals
+    Identifier,      // Identifier
+    ControlKeyword,  // If
+    Keyword,         // Implements
+    Keyword,         // Import
+    Keyword,         // In
+    Keyword,         // Instanceof
+    Keyword,         // Interface
+    Invalid,         // Invalid
+    Operator,        // LessThan
+    Operator,        // LessThanEquals
+    Keyword,         // Let
+    Operator,        // Minus
+    Operator,        // MinusEquals
+    Operator,        // MinusMinus
+    Keyword,         // New
+    Keyword,         // NullLiteral
+    Number,          // NumericLiteral
+    Keyword,         // Package
+    Punctuation,     // ParenClose
+    Punctuation,     // ParenOpen
+    Operator,        // Percent
+    Operator,        // PercentEquals
+    Operator,        // Period
+    Operator,        // Pipe
+    Operator,        // PipeEquals
+    Operator,        // Plus
+    Operator,        // PlusEquals
+    Operator,        // PlusPlus
+    Keyword,         // Private
+    Identifier,      // PrivateIdentifier
+    Keyword,         // Protected
+    Keyword,         // Public
+    Operator,        // QuestionMark
+    Operator,        // QuestionMarkPeriod
+    String,          // RegexFlags
+    String,          // RegexLiteral
+    ControlKeyword,  // Return
+    Punctuation,     // Semicolon
+    Operator,        // ShiftLeft
+    Operator,        // ShiftLeftEquals
+    Operator,        // ShiftRight
+    Operator,        // ShiftRightEquals
+    Operator,        // Slash
+    Operator,        // SlashEquals
+    Keyword,         // Static
+    String,          // StringLiteral
+    Keyword,         // Super
+    ControlKeyword,  // Switch
+    String,          // TemplateLiteralEnd
+    Punctuation,     // TemplateLiteralExprEnd
+    Punctuation,     // TemplateLiteralExprStart
+    String,          // TemplateLiteralStart
+    String,          // TemplateLiteralString
+    Keyword,         // This
+    ControlKeyword,  // Throw
+    Operator,        // Tilde
+    Operator,        // TripleDot
+    Trivia,          // Trivia
+    ControlKeyword,  // Try
+    Keyword,         // Typeof
+    Operator,        // UnsignedShiftRight
+    Operator,        // UnsignedShiftRightEquals
+    String,          // UnterminatedRegexLiteral
+    String,          // UnterminatedStringLiteral
+    String,          // UnterminatedTemplateLiteral
+    Keyword,         // Var
+    Keyword,         // Void
+    ControlKeyword,  // While
+    ControlKeyword,  // With
+    ControlKeyword,  // Yield
+];
+
+// Indexed by TokenType as u8. Order must match the TokenType enum.
+const TOKEN_NAMES: [&str; TOKEN_COUNT] = [
+    "Ampersand",
+    "AmpersandEquals",
+    "Arrow",
+    "Asterisk",
+    "AsteriskEquals",
+    "async",
+    "await",
+    "BigIntLiteral",
+    "BoolLiteral",
+    "BracketClose",
+    "BracketOpen",
+    "break",
+    "Caret",
+    "CaretEquals",
+    "case",
+    "catch",
+    "class",
+    "Colon",
+    "Comma",
+    "const",
+    "continue",
+    "CurlyClose",
+    "CurlyOpen",
+    "debugger",
+    "default",
+    "delete",
+    "do",
+    "DoubleAmpersand",
+    "DoubleAmpersandEquals",
+    "DoubleAsterisk",
+    "DoubleAsteriskEquals",
+    "DoublePipe",
+    "DoublePipeEquals",
+    "DoubleQuestionMark",
+    "DoubleQuestionMarkEquals",
+    "else",
+    "enum",
+    "Eof",
+    "Equals",
+    "EqualsEquals",
+    "EqualsEqualsEquals",
+    "EscapedKeyword",
+    "ExclamationMark",
+    "ExclamationMarkEquals",
+    "ExclamationMarkEqualsEquals",
+    "export",
+    "extends",
+    "finally",
+    "for",
+    "function",
+    "GreaterThan",
+    "GreaterThanEquals",
+    "Identifier",
+    "if",
+    "implements",
+    "import",
+    "in",
+    "instanceof",
+    "interface",
+    "Invalid",
+    "LessThan",
+    "LessThanEquals",
+    "let",
+    "Minus",
+    "MinusEquals",
+    "MinusMinus",
+    "new",
+    "null",
+    "NumericLiteral",
+    "package",
+    "ParenClose",
+    "ParenOpen",
+    "Percent",
+    "PercentEquals",
+    "Period",
+    "Pipe",
+    "PipeEquals",
+    "Plus",
+    "PlusEquals",
+    "PlusPlus",
+    "private",
+    "PrivateIdentifier",
+    "protected",
+    "public",
+    "QuestionMark",
+    "QuestionMarkPeriod",
+    "RegexFlags",
+    "RegexLiteral",
+    "return",
+    "Semicolon",
+    "ShiftLeft",
+    "ShiftLeftEquals",
+    "ShiftRight",
+    "ShiftRightEquals",
+    "Slash",
+    "SlashEquals",
+    "static",
+    "StringLiteral",
+    "super",
+    "switch",
+    "TemplateLiteralEnd",
+    "TemplateLiteralExprEnd",
+    "TemplateLiteralExprStart",
+    "TemplateLiteralStart",
+    "TemplateLiteralString",
+    "this",
+    "throw",
+    "Tilde",
+    "TripleDot",
+    "Trivia",
+    "try",
+    "typeof",
+    "UnsignedShiftRight",
+    "UnsignedShiftRightEquals",
+    "UnterminatedRegexLiteral",
+    "UnterminatedStringLiteral",
+    "UnterminatedTemplateLiteral",
+    "var",
+    "void",
+    "while",
+    "with",
+    "yield",
+];
+
 impl TokenType {
     pub fn category(self) -> TokenCategory {
-        match self {
-            TokenType::Ampersand
-            | TokenType::AmpersandEquals
-            | TokenType::Arrow
-            | TokenType::Asterisk
-            | TokenType::AsteriskEquals
-            | TokenType::Caret
-            | TokenType::CaretEquals
-            | TokenType::DoubleAmpersand
-            | TokenType::DoubleAmpersandEquals
-            | TokenType::DoubleAsterisk
-            | TokenType::DoubleAsteriskEquals
-            | TokenType::DoublePipe
-            | TokenType::DoublePipeEquals
-            | TokenType::DoubleQuestionMark
-            | TokenType::DoubleQuestionMarkEquals
-            | TokenType::Equals
-            | TokenType::EqualsEquals
-            | TokenType::EqualsEqualsEquals
-            | TokenType::ExclamationMark
-            | TokenType::ExclamationMarkEquals
-            | TokenType::ExclamationMarkEqualsEquals
-            | TokenType::GreaterThan
-            | TokenType::GreaterThanEquals
-            | TokenType::LessThan
-            | TokenType::LessThanEquals
-            | TokenType::Minus
-            | TokenType::MinusEquals
-            | TokenType::MinusMinus
-            | TokenType::Percent
-            | TokenType::PercentEquals
-            | TokenType::Period
-            | TokenType::Pipe
-            | TokenType::PipeEquals
-            | TokenType::Plus
-            | TokenType::PlusEquals
-            | TokenType::PlusPlus
-            | TokenType::QuestionMark
-            | TokenType::QuestionMarkPeriod
-            | TokenType::ShiftLeft
-            | TokenType::ShiftLeftEquals
-            | TokenType::ShiftRight
-            | TokenType::ShiftRightEquals
-            | TokenType::Slash
-            | TokenType::SlashEquals
-            | TokenType::Tilde
-            | TokenType::TripleDot
-            | TokenType::UnsignedShiftRight
-            | TokenType::UnsignedShiftRightEquals => TokenCategory::Operator,
-
-            TokenType::BoolLiteral
-            | TokenType::NullLiteral
-            | TokenType::Async
-            | TokenType::Await
-            | TokenType::Class
-            | TokenType::Const
-            | TokenType::Debugger
-            | TokenType::Delete
-            | TokenType::Enum
-            | TokenType::Export
-            | TokenType::Extends
-            | TokenType::Function
-            | TokenType::Implements
-            | TokenType::Import
-            | TokenType::In
-            | TokenType::Instanceof
-            | TokenType::Interface
-            | TokenType::Let
-            | TokenType::New
-            | TokenType::Package
-            | TokenType::Private
-            | TokenType::Protected
-            | TokenType::Public
-            | TokenType::Static
-            | TokenType::Super
-            | TokenType::This
-            | TokenType::Typeof
-            | TokenType::Var
-            | TokenType::Void => TokenCategory::Keyword,
-
-            TokenType::Break
-            | TokenType::Case
-            | TokenType::Catch
-            | TokenType::Continue
-            | TokenType::Default
-            | TokenType::Do
-            | TokenType::Else
-            | TokenType::Finally
-            | TokenType::For
-            | TokenType::If
-            | TokenType::Return
-            | TokenType::Switch
-            | TokenType::Throw
-            | TokenType::Try
-            | TokenType::While
-            | TokenType::With
-            | TokenType::Yield => TokenCategory::ControlKeyword,
-
-            TokenType::BigIntLiteral | TokenType::NumericLiteral => TokenCategory::Number,
-
-            TokenType::RegexFlags
-            | TokenType::RegexLiteral
-            | TokenType::StringLiteral
-            | TokenType::TemplateLiteralEnd
-            | TokenType::TemplateLiteralString
-            | TokenType::TemplateLiteralStart
-            | TokenType::UnterminatedRegexLiteral
-            | TokenType::UnterminatedStringLiteral
-            | TokenType::UnterminatedTemplateLiteral => TokenCategory::String,
-
-            TokenType::BracketClose
-            | TokenType::BracketOpen
-            | TokenType::Colon
-            | TokenType::Comma
-            | TokenType::CurlyClose
-            | TokenType::CurlyOpen
-            | TokenType::ParenClose
-            | TokenType::ParenOpen
-            | TokenType::Semicolon
-            | TokenType::TemplateLiteralExprEnd
-            | TokenType::TemplateLiteralExprStart => TokenCategory::Punctuation,
-
-            TokenType::Identifier
-            | TokenType::EscapedKeyword
-            | TokenType::PrivateIdentifier => TokenCategory::Identifier,
-
-            TokenType::Trivia => TokenCategory::Trivia,
-
-            TokenType::Eof | TokenType::Invalid => TokenCategory::Invalid,
-        }
+        TOKEN_CATEGORIES[self as usize]
     }
 
     pub fn name(self) -> &'static str {
-        match self {
-            TokenType::Ampersand => "Ampersand",
-            TokenType::AmpersandEquals => "AmpersandEquals",
-            TokenType::Arrow => "Arrow",
-            TokenType::Asterisk => "Asterisk",
-            TokenType::AsteriskEquals => "AsteriskEquals",
-            TokenType::Async => "async",
-            TokenType::Await => "await",
-            TokenType::BigIntLiteral => "BigIntLiteral",
-            TokenType::BoolLiteral => "BoolLiteral",
-            TokenType::BracketClose => "BracketClose",
-            TokenType::BracketOpen => "BracketOpen",
-            TokenType::Break => "break",
-            TokenType::Caret => "Caret",
-            TokenType::CaretEquals => "CaretEquals",
-            TokenType::Case => "case",
-            TokenType::Catch => "catch",
-            TokenType::Class => "class",
-            TokenType::Colon => "Colon",
-            TokenType::Comma => "Comma",
-            TokenType::Const => "const",
-            TokenType::Continue => "continue",
-            TokenType::CurlyClose => "CurlyClose",
-            TokenType::CurlyOpen => "CurlyOpen",
-            TokenType::Debugger => "debugger",
-            TokenType::Default => "default",
-            TokenType::Delete => "delete",
-            TokenType::Do => "do",
-            TokenType::DoubleAmpersand => "DoubleAmpersand",
-            TokenType::DoubleAmpersandEquals => "DoubleAmpersandEquals",
-            TokenType::DoubleAsterisk => "DoubleAsterisk",
-            TokenType::DoubleAsteriskEquals => "DoubleAsteriskEquals",
-            TokenType::DoublePipe => "DoublePipe",
-            TokenType::DoublePipeEquals => "DoublePipeEquals",
-            TokenType::DoubleQuestionMark => "DoubleQuestionMark",
-            TokenType::DoubleQuestionMarkEquals => "DoubleQuestionMarkEquals",
-            TokenType::Else => "else",
-            TokenType::Enum => "enum",
-            TokenType::Eof => "Eof",
-            TokenType::Equals => "Equals",
-            TokenType::EqualsEquals => "EqualsEquals",
-            TokenType::EqualsEqualsEquals => "EqualsEqualsEquals",
-            TokenType::EscapedKeyword => "EscapedKeyword",
-            TokenType::ExclamationMark => "ExclamationMark",
-            TokenType::ExclamationMarkEquals => "ExclamationMarkEquals",
-            TokenType::ExclamationMarkEqualsEquals => "ExclamationMarkEqualsEquals",
-            TokenType::Export => "export",
-            TokenType::Extends => "extends",
-            TokenType::Finally => "finally",
-            TokenType::For => "for",
-            TokenType::Function => "function",
-            TokenType::GreaterThan => "GreaterThan",
-            TokenType::GreaterThanEquals => "GreaterThanEquals",
-            TokenType::Identifier => "Identifier",
-            TokenType::If => "if",
-            TokenType::Implements => "implements",
-            TokenType::Import => "import",
-            TokenType::In => "in",
-            TokenType::Instanceof => "instanceof",
-            TokenType::Interface => "interface",
-            TokenType::Invalid => "Invalid",
-            TokenType::LessThan => "LessThan",
-            TokenType::LessThanEquals => "LessThanEquals",
-            TokenType::Let => "let",
-            TokenType::Minus => "Minus",
-            TokenType::MinusEquals => "MinusEquals",
-            TokenType::MinusMinus => "MinusMinus",
-            TokenType::New => "new",
-            TokenType::NullLiteral => "null",
-            TokenType::NumericLiteral => "NumericLiteral",
-            TokenType::Package => "package",
-            TokenType::ParenClose => "ParenClose",
-            TokenType::ParenOpen => "ParenOpen",
-            TokenType::Percent => "Percent",
-            TokenType::PercentEquals => "PercentEquals",
-            TokenType::Period => "Period",
-            TokenType::Pipe => "Pipe",
-            TokenType::PipeEquals => "PipeEquals",
-            TokenType::Plus => "Plus",
-            TokenType::PlusEquals => "PlusEquals",
-            TokenType::PlusPlus => "PlusPlus",
-            TokenType::Private => "private",
-            TokenType::PrivateIdentifier => "PrivateIdentifier",
-            TokenType::Protected => "protected",
-            TokenType::Public => "public",
-            TokenType::QuestionMark => "QuestionMark",
-            TokenType::QuestionMarkPeriod => "QuestionMarkPeriod",
-            TokenType::RegexFlags => "RegexFlags",
-            TokenType::RegexLiteral => "RegexLiteral",
-            TokenType::Return => "return",
-            TokenType::Semicolon => "Semicolon",
-            TokenType::ShiftLeft => "ShiftLeft",
-            TokenType::ShiftLeftEquals => "ShiftLeftEquals",
-            TokenType::ShiftRight => "ShiftRight",
-            TokenType::ShiftRightEquals => "ShiftRightEquals",
-            TokenType::Slash => "Slash",
-            TokenType::SlashEquals => "SlashEquals",
-            TokenType::Static => "static",
-            TokenType::StringLiteral => "StringLiteral",
-            TokenType::Super => "super",
-            TokenType::Switch => "switch",
-            TokenType::TemplateLiteralEnd => "TemplateLiteralEnd",
-            TokenType::TemplateLiteralExprEnd => "TemplateLiteralExprEnd",
-            TokenType::TemplateLiteralExprStart => "TemplateLiteralExprStart",
-            TokenType::TemplateLiteralStart => "TemplateLiteralStart",
-            TokenType::TemplateLiteralString => "TemplateLiteralString",
-            TokenType::This => "this",
-            TokenType::Throw => "throw",
-            TokenType::Tilde => "Tilde",
-            TokenType::TripleDot => "TripleDot",
-            TokenType::Trivia => "Trivia",
-            TokenType::Try => "try",
-            TokenType::Typeof => "typeof",
-            TokenType::UnsignedShiftRight => "UnsignedShiftRight",
-            TokenType::UnsignedShiftRightEquals => "UnsignedShiftRightEquals",
-            TokenType::UnterminatedRegexLiteral => "UnterminatedRegexLiteral",
-            TokenType::UnterminatedStringLiteral => "UnterminatedStringLiteral",
-            TokenType::UnterminatedTemplateLiteral => "UnterminatedTemplateLiteral",
-            TokenType::Var => "var",
-            TokenType::Void => "void",
-            TokenType::While => "while",
-            TokenType::With => "with",
-            TokenType::Yield => "yield",
-        }
+        TOKEN_NAMES[self as usize]
     }
 
     pub fn is_identifier_name(self) -> bool {
