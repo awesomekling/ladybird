558c05fe561fcef120347c29c30058210cdaf218
Andreas Kling <andreas@ladybird.org>
2026-02-20 19:25:27 +0100

    LibJS: Add bytecode test for regex literal inside a function
    Exercise the ownership transfer path where a compiled regex handle
    travels through the FunctionPayload round-trip to C++.

--- Tests/LibJS/Bytecode/input/regex-literals.js
+++ Tests/LibJS/Bytecode/input/regex-literals.js
@@ -2,3 +2,9 @@
 let a = /foo/;
 let b = /bar/gi;
 let c = 1 + /baz/i;
+
+// Regex inside a function (exercises ownership transfer through FunctionPayload)
+function matchDigits(s) {
+    return /\d+/g.test(s);
+}
+matchDigits("abc123");
